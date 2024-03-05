0f62d39a2b79c641e660e4475114cb6d3e7a748d
rthomas <ryan.thomas39500@gmail.com>
2024-03-05 15:51:20 +0100

    refacto image service

--- back/core/services/image/image_service.py
+++ back/core/services/image/image_service.py
@@ -1,21 +1,21 @@
 # core/image_service.py
 from typing import List
 from adapters.image.image_repository import ImageRepository
-from core.services.i_image_service import IImageService
+from core.services.image.i_image_service import IImageService
 from core.entities.image import Image
 
 class ImageService(IImageService):
     def __init__(self, image_repository: ImageRepository):
         self.image_repository = image_repository
 
-    def create_image(self, image: str, name: str, size: int) -> Image:
-        return self.image_repository.create_image(image, name, size)
+    def create_image(self, image: str, name: str, size: int, library_id: int) -> Image:
+        return self.image_repository.create_image(image, name, size, library_id)
 
     def get_image_by_id(self, image_id: int) -> Image:
         return self.image_repository.get_image_by_id(image_id)
 
-    def get_all_images(self) -> List[Image]:
-        return self.image_repository.get_all_images()
+    def get_all_images(self, library_id) -> List[Image]:
+        return self.image_repository.get_all_images(library_id)
     
     def delete_image(self, image_id: int):
         return self.image_repository.delete_image(image_id)
