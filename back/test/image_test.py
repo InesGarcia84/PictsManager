4b8e72922cd777176c27076c6dff38154e400dfe
Inès Garcia <ines.garcia@epitech.eu>
2024-03-19 17:13:49 +0100

    add some tests for entities library and image

---
+++ back/test/image_test.py
@@ -0,0 +1,41 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+fake_image = {
+    "id": 3,
+    "size": 150,
+    "library_id": 1,
+    "name": "stra3",
+    "image": "stra2",
+    "tags": None,
+}
+
+
+def test_get_image():
+    response = client.get("/api/image/3", cookies={"session": "example"})
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": fake_image["id"],
+        "size": fake_image["size"],
+        "library_id":fake_image["library_id"],
+        "name": fake_image["name"],
+        "image": fake_image["image"],
+        "tags": fake_image["tags"]
+    }
+
+def test_get_images_library_1():
+    response = client.get("/api/images/library/1", cookies={"session": "example"})
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "id": fake_image["id"],
+            "size": fake_image["size"],
+            "library_id":fake_image["library_id"],
+            "name": fake_image["name"],
+            "image": fake_image["image"],
+            "tags": fake_image["tags"]
+        }
+    ]
+                               
